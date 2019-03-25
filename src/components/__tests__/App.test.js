41fa80d81ca223c07e43abd2d0d1de0489572b6f
Pouria <charliswager@gmail.com>
2019-03-26 00:51:38 +0430

    write a expect test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,5 +6,7 @@ it("shows a comment box", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 
+  expect(div.innerHTML).toContain("Comment Box");
+
   ReactDOM.unmountComponentAtNode(div);
 });
