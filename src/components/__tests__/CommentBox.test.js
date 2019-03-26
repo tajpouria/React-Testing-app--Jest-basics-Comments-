0855ab70458c4f6da55f374c79203732f42968b1
Pouria <charliswager@gmail.com>
2019-03-27 00:51:19 +0430

    to testing the function of a textare a used these steps : 1.i simulate an change  on the text area using wrrapped.find "textarea" . simulate "change" , {target:{value:"newcomment"}} and then used wrapped.update to rerender the component after that i wrote my expect like this expect wrapper.find "textarea" . prop "value" .toEqual "newcomment"

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,11 @@ it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
+
+it("has a text area that user can type in", () => {
+  wrapped
+    .find("textarea")
+    .simulate("change", { target: { value: "new comment" } });
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
