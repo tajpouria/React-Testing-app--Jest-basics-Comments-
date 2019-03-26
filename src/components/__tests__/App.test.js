c511508b5bdeabd4f3b373c20e40c5fa07fde2e6
Pouria <charliswager@gmail.com>
2019-03-26 15:05:07 +0430

    add a file .env into root and write NODE_PATH=src/ to have path from src

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "../App";
-import CommentBox from "../CommentBox";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
+
 it("shows a comment box", () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find(CommentBox).length).toEqual(1);
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list ", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
