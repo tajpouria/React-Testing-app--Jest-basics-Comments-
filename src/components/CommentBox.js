7c93ff6576dfc04ce9d4ca13c8e32b268c7bfe32
Pouria <charliswager@gmail.com>
2019-03-26 17:39:45 +0430

    make a seperade file name commentbox.tets.js it used for writing tests too mount methode work exactlly like shallow methode but it s also find children

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-  return <div>Comment Box</div>;
-};
+export default class extends Component {
+  state = {
+    comment: ""
+  };
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // TODO call and action creator
+    // save the comment
+
+    this.setState({ comment: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4> Add a comment </h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+    );
+  }
+}
