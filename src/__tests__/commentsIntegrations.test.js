5dc9ba2b8e7b66895c99595164b3f28e46309c4c
Pouria <charliswager@gmail.com>
2019-03-28 02:20:59 +0430

    i added a class to my fetch button so i can find it easier using .classname and it seems like we have problem to find li cuz we receive 0

--- src/__tests__/commentsIntegrations.test.js
+++ src/__tests__/commentsIntegrations.test.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { map } from "enzyme";
+import { mount } from "enzyme";
 import Root from "Root";
 import App from "components/App";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <App />
+    </Root>
+  );
+});
 
 it("can fetch a list of comments and display it", () => {
   // Attempt to render whole app
   // find the fetchComments button and click it
+
+  wrapped.find(".fetch-comments").simulate("click");
+
   // expect to find a list of comments
+  wrapped.update();
+  expect(wrapped.find("li").length).toEqual(500);
 });
