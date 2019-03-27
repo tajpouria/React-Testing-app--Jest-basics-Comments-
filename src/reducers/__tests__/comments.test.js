66d5f90783ab1f9e095030d70c1dd6b8595f7cd1
Pouria <charliswager@gmail.com>
2019-03-27 22:05:44 +0430

    i also write a test with a random action type

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -9,3 +9,11 @@ it("reducer return comments correctly", () => {
   const newState = commentsReducer([], action);
   expect(newState.comments).toEqual(["new comment"]);
 });
+
+it("handle action with unknow action.", () => {
+  const newState = commentsReducer([], {
+    type: "random",
+    payload: "another comment"
+  });
+  expect(newState.comments).toEqual([]);
+});
