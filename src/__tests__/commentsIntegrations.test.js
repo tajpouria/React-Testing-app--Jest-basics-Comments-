21d51676d83970b5cc7b8b7e6b864add6baeac7e
Pouria <charliswager@gmail.com>
2019-03-28 01:53:46 +0430

    make some builer play for first integrations test

---
+++ src/__tests__/commentsIntegrations.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { map } from "enzyme";
+import Root from "Root";
+import App from "components/App";
+
+it("can fetch a list of comments and display it", () => {
+  // Attempt to render whole app
+  // find the fetchComments button and click it
+  // expect to find a list of comments
+});
