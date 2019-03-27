11588d04bbcba8e79ed488097efa76b17aaaec0d
Pouria <charliswager@gmail.com>
2019-03-28 01:11:50 +0430

    used redux-promise as middleware and connect it to my store

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
 import reducer from "reducers";
 
 export default ({ children, initialState = {} }) => {
-  return (
-    <Provider store={createStore(reducer, initialState)}>{children}</Provider>
+  const store = createStore(
+    reducer,
+    initialState,
+    applyMiddleware(reduxPromise)
   );
+  return <Provider store={store}>{children}</Provider>;
 };
