cf292d183a4045ef0db80343e21cdf1fbb373ddb
Pouria <charliswager@gmail.com>
2019-03-28 02:55:04 +0430

    i doing these steps to test recive and desplay fetch comments : 1. after setup wrapped and simulate click i try to find li but i receved 0 thats cuz we are out of real dom we just got fake console dom to solving these issue i used maxios libreary => 2.maxios is made for testing app and it will send fake response to axios req after setup in beforeEach and afterEach we can use it 3. we also need a tiny delay to let maxios sen the respose and i provide it using setTimeout 4.i also used done methode from jest it will done the test when all functions executed

--- src/__tests__/commentsIntegrations.test.js
+++ src/__tests__/commentsIntegrations.test.js
@@ -1,25 +1,33 @@
 import React from "react";
 import { mount } from "enzyme";
+import moxios from "moxios";
 import Root from "Root";
 import App from "components/App";
 
 let wrapped;
 
 beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest("http://jsonplaceholder.typicode.com/comments", {
+    status: 200,
+    response: [{ name: "fetch #1" }, { name: "fetch #2" }]
+  });
   wrapped = mount(
     <Root>
       <App />
     </Root>
   );
 });
+afterEach(() => {
+  moxios.uninstall();
+});
 
-it("can fetch a list of comments and display it", () => {
-  // Attempt to render whole app
-  // find the fetchComments button and click it
-
+it("can fetch a list of comments and display it", done => {
   wrapped.find(".fetch-comments").simulate("click");
 
-  // expect to find a list of comments
-  wrapped.update();
-  expect(wrapped.find("li").length).toEqual(500);
+  setTimeout(() => {
+    wrapped.update();
+    expect(wrapped.find("li").length).toEqual(2);
+    done();
+  }, 100);
 });
