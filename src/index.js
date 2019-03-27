4867383ef452d63a3d80950c9cfa0e86391e52ec
Pouria <charliswager@gmail.com>
2019-03-27 13:42:59 +0430

    refresh provider store and so on

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "reducers";
 
 import App from "components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={createStore(reducer, {})}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
