02bc1630973a5618d57092f2a9bb6ddaac49a9f0
Pouria <charliswager@gmail.com>
2019-03-27 22:47:40 +0430

    modify my comment list component also setup mapStateToProps to receive props

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default () => {
-  return <div>Comment List</div>;
+class CommentList extends Component {
+  renderList() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <ul>{this.renderList()}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    comments: state.comments
+  };
 };
+
+export default connect(mapStateToProps)(CommentList);
