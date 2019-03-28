4cbb508802a79578977132b39e9c7fa1a2cdb319
Pouria <charliswager@gmail.com>
2019-03-28 15:46:28 +0430

    auth reducer and auth action creator added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from "actions/types";
+import { CHANGE_AUTH, SAVE_COMMENT, FETCH_COMMENTS } from "actions/types";
 import axios from "axios";
 
 export function saveComment(comment) {
@@ -13,5 +13,12 @@ export function fetchComments() {
   return {
     type: FETCH_COMMENTS,
     payload: comments
+  };
+}
+
+export function changeAuth(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
   };
 }
