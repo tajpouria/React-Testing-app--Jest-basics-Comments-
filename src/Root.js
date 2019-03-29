68ddb43b482df17908ad9a76b8280ab5e4461df9
Pouria <charliswager@gmail.com>
2019-03-29 15:45:27 +0430

    debuging and add a warn for invalid actions

--- src/Root.js
+++ src/Root.js
@@ -2,9 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducer from "reducers";
+import stateValidate from "middlewares/stateValidate";
 import async from "middlewares/async";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducer, initialState, applyMiddleware(async));
+  const store = createStore(
+    reducer,
+    initialState,
+    applyMiddleware(async, stateValidate)
+  );
   return <Provider store={store}>{children}</Provider>;
 };
